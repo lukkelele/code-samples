d48ce401742cecc246fd7c641d6c547c77821874
Lukas Gunnarsson <lukasgunnarsson@protonmail.com>
2026-02-22 14:13:47 +0100

    cmake: extensions: add macros/functions

--- cmake/extensions.cmake
+++ cmake/extensions.cmake
@@ -4,15 +4,40 @@
 
 # Print sample banner.
 function(sample_print_banner)
-    message(STATUS "
-==================================================
- Sample: ${PROJECT_NAME}
-==================================================")
+    message(STATUS "==================================================
+    Sample: ${PROJECT_NAME}
+   ==================================================")
 endfunction()
 
 # Finalize a sample by setting up properties and other things to
 # make it easier to run using 'Tasks' (with Zed/Visual Studio Code).
 macro(sample_finalize)
+    set(output_dir ${CMAKE_BINARY_DIR}/app)
+    set_target_properties(${PROJECT_NAME} PROPERTIES
+        RUNTIME_OUTPUT_DIRECTORY ${output_dir}
+        RUNTIME_OUTPUT_DIRECTORY_DEBUG ${output_dir}
+        RUNTIME_OUTPUT_DIRECTORY_RELEASE ${output_dir}
+        RUNTIME_OUTPUT_DIRECtORY_RELWITHDEBINFO ${output_dir}
+        RUNTIME_OUTPUT_DIRECTORY_MINSIZEREL ${output_dir}
+    )
+
+    add_custom_command(
+        TARGET ${PROJECT_NAME}
+        POST_BUILD
+        COMMAND ${CMAKE_COMMAND} -E create_hardlink
+                $<TARGET_FILE:${PROJECT_NAME}>
+                $<TARGET_FILE_DIR:${PROJECT_NAME}>/app
+    )
+endmacro()
+
+# Print experimental project banner.
+function(experimental_print_banner)
+    message(STATUS "==================================================
+    ${PROJECT_NAME}
+   ==================================================")
+endfunction()
+
+macro(experimental_finalize)
     set(output_dir ${CMAKE_BINARY_DIR}/app)
     set_target_properties(${PROJECT_NAME} PROPERTIES
         RUNTIME_OUTPUT_DIRECTORY ${output_dir}
