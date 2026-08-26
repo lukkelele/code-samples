4e3da10d267f3f59fc77b6e887dd10bcf34d3f84
lukkelele <lukasgunnarsson@protonmail.com>
2026-08-26 17:32:15 +0200

    cpp: gui: profiler_timer: add sample

---
+++ samples/cpp/gui/profiler_timer/main.cpp
@@ -0,0 +1,44 @@
+#include <cstdio>
+
+#include "core/profiler.h"
+#include "gui/window.h"
+
+int main(const int argc, char* const* argv)
+{
+	core::init(lklog::level::trace);
+
+	using namespace gui;
+	const WindowSpecification window_spec = {
+		.width = 1024,
+		.height = 800,
+		.title = SAMPLE_NAME};
+	auto& window = CWindow::get();
+	window.init(window_spec);
+
+	PROFILER_THREAD("Main");
+
+	LOG_INFO("Enter main loop");
+	while (window.is_running()) {
+		PROFILER_SCOPED("Main Loop");
+		window.begin_frame();
+
+		if (ImGui::Begin("Window")) {
+			PROFILER_SCOPED("Window");
+			if (ImGui::Button("Button")) {
+				constexpr auto SLEEP_DELAY = 800ms;
+				LOG_INFO("Sleeping for {}", SLEEP_DELAY);
+				std::this_thread::sleep_for(SLEEP_DELAY);
+			}
+			ImGui::End();
+		}
+
+		/* Slow down the execution a bit. */
+		std::this_thread::sleep_for(3ms);
+
+		window.end_frame();
+		PROFILER_MARK_FRAME();
+	}
+
+	window.destroy();
+	return 0;
+}
